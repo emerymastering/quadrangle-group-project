be52edf9bb1d1f6a3a10dc4a3122d9ee5c14190f
emerymastering <emery@mastering.lt>
2022-11-07 18:04:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class=" min-h-screen flex items-center justify-center bg-indigo-900">
+        <div class="relative">
+          <div class="w-16 h-16 bg-white rounded-lg shadow-2xl"></div>
+          <div class="absolute top-0 right-0 -mr-1 -mt-1 w-4 h-4 rounded-full bg-green-300 animate-ping">
+            <h1 className="text-3xl font-bold underline pt-40">
+              Welcome to Quadrangle!
+            </h1>
+          </div>
+          <div class="absolute top-0 right-0 -mr-1 -mt-1 w-4 h-4 rounded-full bg-green-300">
+            <h1 className="text-3xl font-bold underline pt-40">
+              Welcome to Quadrangle!
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
