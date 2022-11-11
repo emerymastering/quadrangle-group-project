e3e091bbd2a9dd4a607ee89fd05d37e98892fac7
emerymastering <emery@mastering.lt>
2022-11-11 17:44:58 +0100

    initial setup for blinking image

--- src/pages/Denis.js
+++ src/pages/Denis.js
@@ -1,3 +1,25 @@
+import { useState, useEffect } from "react";
+
 export const Denis = () => {
-  return;
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowComponent(!showComponent);
+    }, 2000);
+  }, [showComponent]);
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      {showComponent && (
+        <div>
+          <img
+            src="https://mdbootstrap.com//img/Photos/Square/1.jpg"
+            className="max-w-xs h-auto rounded-full"
+            alt=""
+          ></img>
+        </div>
+      )}
+    </div>
+  );
 };
