666a2b6555a6e900fb9af9f4dafe55e5712ab0bf
emerymastering <emery@mastering.lt>
2022-11-12 20:52:20 +0100

    adding start game button and some styling

--- src/pages/Denis.js
+++ src/pages/Denis.js
@@ -6,20 +6,17 @@ export const Denis = () => {
   const [points, setPoints] = useState(0);
   const [time, setTime] = useState(3);
   const [gameFinished, setGameFinished] = useState(false);
+  const [gameStart, setGameStart] = useState(false);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setShowComponent(!showComponent);
-    }, 2000);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [showComponent]);
-
+  const onStartClick = () => {
+    setGameStart(true);
+    setGameFinished(false);
+  };
   const shownFood = imagesFood[Math.floor(Math.random() * imagesFood.length)];
   const onClick = () => {
     setPoints(points + (shownFood.type === "v" ? 5 : -5));
     setShowComponent(!showComponent);
+    setGameStart(true);
   };
 
   useEffect(() => {
@@ -34,25 +31,59 @@ export const Denis = () => {
     };
   }, [time]);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setShowComponent(!showComponent);
+    }, 2000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [showComponent]);
+
   return (
-    <div className="flex items-center justify-center h-screen bg-[url('./images/vs.jpeg')] bg-center bg-cover">
-      {gameFinished ? (
-        <div>Game Over, your score is: {points}</div>
+    <div className="flex mx-auto justify-center h-screen bg-[url('./images/vs.jpeg')] bg-center bg-cover">
+      {!gameStart ? (
+        <button
+          onClick={() => onStartClick()}
+          className="bg-black text-5xl text-red-300 px-10 py-10 my-auto rounded-full"
+        >
+          Start Eating Healthy
+        </button>
       ) : (
-        <div>
-          <div>Score:{points}</div>
-          {"_"}
-          <div>Time:{time}</div>
-          {
-            <div>
-              <img
-                src={shownFood.image}
-                className="max-w-xs h-auto rounded-full"
-                alt=""
-                onClick={() => onClick()}
-              ></img>
+        <div className="my-auto">
+          {gameFinished && gameStart ? (
+            <div className="flex flex-col">
+              <div className="bg-black animate-pulse text-5xl text-red-300 px-10 py-10 rounded-full">
+                Game Over, you scored {points} health points
+              </div>
+              <button
+                onClick={() => window.location.reload(false)}
+                className="bg-black text-5xl text-red-300 py-10 mx-40 mt-4 rounded-full"
+              >
+                Start Again
+              </button>
             </div>
-          }
+          ) : (
+            <div className="flex flex-col mt-0">
+              <div className="px-10 bg-black text-5xl text-red-300 py-10 mx-40 mt-4 rounded-full">
+                Health points: {points}
+              </div>
+
+              <div className="px-10 bg-black text-5xl text-red-300 py-10 mx-40 mt-4 mb-20 rounded-full text-center">
+                Time: {time} sec
+              </div>
+              {
+                <div className="max-h-10 mx-auto">
+                  <img
+                    src={shownFood.image}
+                    className="max-w-xs h-auto rounded-full"
+                    alt=""
+                    onClick={() => onClick()}
+                  ></img>
+                </div>
+              }
+            </div>
+          )}
         </div>
       )}
     </div>
