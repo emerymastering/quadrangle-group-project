f02e57cdad4af746e4d6ea00ff88af60d04c56b1
chathu-weerasena <chathuweerasena@gmail.com>
2022-11-13 22:15:22 +0100

    Completed Keyboard design

--- src/componenets/KeyBoard/Key.js
+++ src/componenets/KeyBoard/Key.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Key(props) {
   if (props.big) {
     <button
-      onClick={() => handleClickForBig()}
+      //onClick={() => handleClickForBig()}
       style={{
         width: 65.4,
         height: 58,
@@ -23,5 +23,26 @@ export default function Key(props) {
       {props.letter}
     </button>;
   }
-  return <div>Key</div>;
+  return (
+    <button
+      // onClick={() => guessTheWord(props.letter)}
+      style={{
+        width: 43,
+        height: 58,
+        margin: 3,
+        borderRadius: 3,
+        display: "grid",
+        placeItems: "center",
+        fontSize: 15,
+        backgroundColor: "#d3d6da",
+        color: "black",
+        fontFamily: "Arial",
+        cursor: "pointer",
+        border: 0,
+        fontWeight: "bold",
+      }}
+    >
+      {props.letter}
+    </button>
+  );
 }
